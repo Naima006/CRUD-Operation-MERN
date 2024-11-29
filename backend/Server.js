fa57cd5445f3708f22e83537b623a821f2b1e66a
Naima Rahman <naimarahman006@gmail.com>
2024-11-30 01:40:32 +0600

    Updating Server.js and vercel.json

--- backend/Server.js
+++ backend/Server.js
@@ -11,24 +11,19 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-// MongoDB Connection
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected..."))
     .catch((err) => console.log(err));
 
-// Routes
 app.use("/api", routes);
 
-// Fallback Route for Undefined Routes
 app.get("/", (req, res) => {
     res.send("Welcome to the CRUD API!");
 });
 
-// Handle Undefined API Routes
 app.use((req, res) => {
     res.status(404).send("Route not found.");
 });
 
-// Start Server
 app.listen(PORT, () => console.log(`Listening at ${PORT}`));
